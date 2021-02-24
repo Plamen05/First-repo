77ba1dabb737f393464c96cc45494bdfd7fddc7b
Plamen05 <78840790+Plamen05@users.noreply.github.com>
2021-02-24 11:01:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10kam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете десетично число");
+            int chislo = int.Parse(Console.ReadLine());
+            string chislo2 = Convert.ToString(chislo, 2);
+            Console.WriteLine("10ichno: " + chislo);
+            Console.WriteLine("2ichno: " + chislo2);
+            string chislo16 = Convert.ToString(chislo, 16);
+            Console.WriteLine("16isetichno: " + chislo16);
+        }
+    }
+}
